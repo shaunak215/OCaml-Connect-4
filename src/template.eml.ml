50ca4843721b7a27a5c23329fa26803a0aa45181
kyleswu <kwu45@jhu.edu>
2021-12-11 18:47:29 -0500

    html template file for server

--- src/template.eml.ml
+++ src/template.eml.ml
@@ -1,6 +1,24 @@
-let render param =
+let game_over winner =
   <html>
   <body>
-    <h1>Render the board: <%s param %></h1>
+  <h1>Winner: <%s winner%></h1>
+  <body>
+  <html>
+
+let game_in_progress ?message board player request =
+  <html>
+  <body>
+  <h1>board: <%s board%></h1>
+  <h1><%s player%>'s turn</h1>
+%   begin match message with
+%   | None -> ()
+%   | Some message ->
+      <p>You entered: <b><%s message %>!</b></p>
+%   end;
+
+    <%s! Dream.form_tag ~action:"/" request %>
+      <input name="message" autofocus>
+    </form>
+
   </body>
   </html>
