7904bdf53a721873bea5730ae83769eccd2aee0b
Shaunak <shaunak215@gmail.com>
2021-12-11 16:32:14 -0500

    ai caching

--- src/ai.ml
+++ src/ai.ml
@@ -1,9 +1,15 @@
-[@@@ocaml.warning "-27"]
-
-[@@@ocaml.warning "-33"]
-
 open Board
+open Game
 open Core
+
+let min_score = -(42 / 2) + 3
+
+(* let max_score = ((42 + 1) / 2) - 3 *)
+
+let table =
+  Hashtbl.create
+    (module String)
+    ?growth_allowed:(Some false) ?size:(Some 8388593)
 
 let count_moves (b : Board.t) : int =
   let l, _ = b in
@@ -14,33 +20,8 @@ let is_winning_move (b : Board.t) (col : int) : bool =
   let res, _ = game_over col new_b in
   res
 
-(* code attempt at alpha beta *)
-
-(* let rec negmax (b : Board.t) (alpha : int) (beta : int) : int =
-     let moves = count_moves b in
-     if moves = 42 then 0
-     else if
-       List.for_all [ 1; 2; 3; 4; 5; 6; 7 ] ~f:(fun el ->
-           if is_valid_move el b && is_winning_move b el then false else true)
-     then
-       let max = (42 + 1 - moves) / 2 in
-       if beta > max && alpha >= max then max
-       else if beta > max then negmax_helper b alpha max 1
-       else negmax_helper b alpha beta 1
-     else (42 + 1 - moves) / 2
-
-   and negmax_helper (b : Board.t) (alpha : int) (beta : int) (col : int) : int =
-     if col = 8 then alpha
-     else if is_valid_move col b then
-       let new_b = insert_piece col b in
-       let score = -negmax new_b (-alpha) (-beta) in
-       if score >= beta then score
-       else if score > alpha then negmax_helper b score beta (col + 1)
-       else negmax_helper b alpha beta (col + 1)
-     else negmax_helper b alpha beta (col + 1) *)
-
-let rec negmax (b : Board.t) (depth : int) : int =
-  if depth > 5 then 0
+let rec negmax (b : Board.t) (depth : int) (alpha : int) (beta : int) : int =
+  if depth > 10 then 0
   else
     let moves = count_moves b in
     if moves = 42 then 0
@@ -48,28 +29,40 @@ let rec negmax (b : Board.t) (depth : int) : int =
       List.for_all [ 1; 2; 3; 4; 5; 6; 7 ] ~f:(fun el ->
           if is_valid_move el b && is_winning_move b el then false else true)
     then
-      let best_score = -42 in
-      negmax_helper best_score [ 4; 3; 5; 2; 6; 1; 7 ] b depth
+      let key = encode_game b in
+      let v = Hashtbl.find table key in
+      let max =
+        if is_none v then (42 - 1 - moves) / 2
+        else Option.value_exn v + min_score - 1
+      in
+      if beta > max && alpha >= max then max
+      else if beta > max then
+        negmax_helper b alpha max [ 4; 3; 5; 2; 6; 1; 7 ] depth
+      else negmax_helper b alpha beta [ 4; 3; 5; 2; 6; 1; 7 ] depth
     else (42 + 1 - moves) / 2
 
-and negmax_helper (score : int) (col : int list) (b : Board.t) (depth : int) :
-    int =
+and negmax_helper (b : Board.t) (alpha : int) (beta : int) (col : int list)
+    (depth : int) : int =
   match col with
-  | [] -> score
+  | [] ->
+      let compress = encode_game b in
+      Hashtbl.update table compress ~f:(fun _ -> alpha - min_score + 1);
+      alpha
   | hd :: tl ->
       if is_valid_move hd b then
         let new_b = insert_piece hd b in
-        let new_score = -negmax new_b (depth + 1) in
-        if new_score > score then negmax_helper new_score tl b depth
-        else negmax_helper score tl b depth
-      else negmax_helper score tl b depth
+        let score = -negmax new_b (depth + 1) (-beta) (-alpha) in
+        if score >= beta then score
+        else if score > alpha then negmax_helper b score beta tl depth
+        else negmax_helper b alpha beta tl depth
+      else negmax_helper b alpha beta tl depth
 
 let solve (b : Board.t) : int =
   let can_win =
-    List.fold [ 1; 2; 3; 4; 5; 6; 7 ] ~init:false ~f:(fun accum el ->
+    List.fold [ 1; 2; 3; 4; 5; 6; 7 ] ~init:false ~f:(fun _ el ->
         if is_winning_move b el then true else false)
   in
-  if can_win then (42 + 1 - count_moves b) / 2 else negmax b 0
+  if can_win then (42 + 1 - count_moves b) / 2 else negmax b 0 (-21) 21
 
 let get_scores (b : Board.t) : int list =
   List.fold [ 1; 2; 3; 4; 5; 6; 7 ] ~init:[] ~f:(fun accum el ->
@@ -82,7 +75,7 @@ let get_scores (b : Board.t) : int list =
   |> List.rev
 
 let get_best_col (l : int list) : int =
-  let col, score =
+  let col, _ =
     List.fold [ 4; 3; 5; 2; 6; 1; 7 ] ~init:(0, -100)
       ~f:(fun (index, max) col ->
         let v = List.nth_exn l (col - 1) in
--- src/game.mli
+++ src/game.mli
@@ -5,17 +5,32 @@
 val new_game : int -> Board.t
 
 (*
-    initializes a saved game with that board
-    1st int value determines number of players 1 (against ai) or 2
+    Takes in a string compression of the board and creates a board from taht
 *)
 val decode_game : string -> Board.t
 
 (*
-    saves the current game state in the form of
-    a string int pair representation of the game
-    1st int value determines number of players 1 (against ai) or 2
+   Takes in a board and creates a string compression of the board.
+   Puts the moves in order of the column they happened in
 *)
 val encode_game : Board.t -> string
+
+(*
+    initializes a saved game with that board
+    1st int value determines number of players 1 (against ai) or 2
+    2nd int value is ignored if the number of players is 1, if there 
+    are two then it indicates if the player is player 1 or 2
+*)
+val load_game : string -> int -> int -> Board.t * int * int
+
+(*
+    saves the current game state in the form of
+    a string int int pair representation of the game
+    1st int value determines number of players 1 (against ai) or 2
+    2nd int value is a dummy value if the number of players is 1, if there 
+    are two then it indicates if the player is player 1 or 2
+*)
+val save_game : Board.t -> int -> int -> string * int * int
 
 (*
     If move is completed return new board and true
