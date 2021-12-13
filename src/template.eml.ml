ae9baac5a57263a19e87d1dddb725ca3513c01f7
Shaunak <shaunak215@gmail.com>
2021-12-13 14:57:14 -0500

    board on all pages

--- src/template.eml.ml
+++ src/template.eml.ml
@@ -12,7 +12,7 @@ let game_over board winner request =
     <div>
     <%s! Dream.form_tag ~action:"/" request %>
         <input type="hidden" name="reset" autofocus>
-        <button type="submit"> Reset </button>
+        <button type="submit" class="reset"> Reset </button>
     </form>
     </div>
     <div class="container">
@@ -90,7 +90,7 @@ let saved board request =
     <div>
     <%s! Dream.form_tag ~action:"/" request %>
         <input type="hidden" name="reset" autofocus>
-        <button type="submit"> Reset </button>
+        <button type="submit" class="reset"> Reset </button>
     </form>
     </div>
     <div class="container">
@@ -158,6 +158,9 @@ let saved board request =
 
 let collect_players ?message request =
   <html>
+  <head>
+    <link rel="stylesheet" href="static/style.css">
+  </head> 
   <body>
     <h1> Welcome to Connect 4! </h1>
     <h2> How many players will be playing? You can also load a game you have already saved.</h2>
@@ -174,12 +177,76 @@ let collect_players ?message request =
         <input type="hidden" name="ending" autofocus>
         <button type="submit"> Load Game </button>
       </form>
+      <div id="board" style="display: none;">""</div>
+      <div class="container">
+      <table>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          <tr>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+              <td class="slot"></td>
+          </tr>
+          </table>
+    </div>
+    <script src="static/app.js"></script>
   </body>
   </html>
 
 let player_order ?message request =
   <html>
+  <head>
+    <link rel="stylesheet" href="static/style.css">
+  </head> 
   <body>
+    <div id="board" style="display: none;">""</div>
     <h1> Do you want to go first or second? </h1>
 
     <%s! Dream.form_tag ~action:"/" request %>
@@ -190,6 +257,65 @@ let player_order ?message request =
       <input type="hidden" name="order" value="2"autofocus>
       <button type="submit"> Second </button>
     </form>
+    <div class="container">
+    <table>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        <tr>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+            <td class="slot"></td>
+        </tr>
+        </table>
+  </div>
+  <script src="static/app.js"></script>
   </body>
   </html>
 
@@ -205,11 +331,13 @@ let game_in_progress ?message board player request =
     <div>
     <%s! Dream.form_tag ~action:"/save" request %>
         <input type="hidden" name="message" autofocus>
-        <button type="submit"> Save </button>
+        <button type="submit" class="save" > Save Game</button>
     </form>
+    </div>
+    <div>
     <%s! Dream.form_tag ~action:"/" request %>
         <input type="hidden" name="reset" autofocus>
-        <button type="submit"> Reset </button>
+        <button type="submit" class="reset"> Reset Game</button>
     </form>
     </div>
     <div class="container">
@@ -274,31 +402,31 @@ let game_in_progress ?message board player request =
       <table>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="1" autofocus>
-        <button class="col-selector"> 1 </button>
+        <button class="col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="2" autofocus>
-        <button class="col-selector"> 2 </button>
+        <button class="col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="3" autofocus>
-        <button class="col-selector"> 3 </button>
+        <button class="col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="4" autofocus>
-        <button class="col-selector"> 4 </button>
+        <button class="col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="5" autofocus>
-        <button class = "col-selector"> 5 </button>
+        <button class = "col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="6" autofocus>
-        <button class = "col-selector"> 6 </button>
+        <button class = "col-selector"> Go Here </button>
       </form>
       <%s! Dream.form_tag ~action:"/play" request %>
         <input type="hidden" id="message" name="message" value="7" autofocus>
-        <button class = "col-selector">7</button>
+        <button class = "col-selector">Go Here</button>
       </form>
       <table>
     </div>
